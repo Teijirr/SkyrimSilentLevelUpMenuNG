46d49d5306dc9962865610acef1ab0d00ba063b5
Teijirr <baba.andre26@gmail.com>
2026-06-07 22:00:09 +0200

    FIrst commit

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,38 +1,47 @@
 #include "log.h"
 
+void SilentLevelUp()
+{
+    if (auto* p = RE::PlayerCharacter::GetSingleton())
+        p->GetPlayerRuntimeData().skills->AdvanceLevel(false);
+}
 
 void OnDataLoaded()
 {
-   
+    SKSE::AllocTrampoline(14);
+
+    const auto addr = REL::RelocationID(51638, 52510).address()
+        + (REL::Module::IsAE() ? 0x1012 : 0xF8E);
+
+    SKSE::GetTrampoline().write_call<5>(addr, SilentLevelUp);
+    SKSE::log::info("NoLevelUpMenu: hook installed.");
 }
 
 void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
 {
-	switch (a_msg->type) {
-	case SKSE::MessagingInterface::kDataLoaded:
-        
-		break;
-	case SKSE::MessagingInterface::kPostLoad:
-		break;
-	case SKSE::MessagingInterface::kPreLoadGame:
-		break;
-	case SKSE::MessagingInterface::kPostLoadGame:
+    switch (a_msg->type) {
+    case SKSE::MessagingInterface::kDataLoaded:
+        OnDataLoaded();
         break;
-	case SKSE::MessagingInterface::kNewGame:
-		break;
-	}
+    case SKSE::MessagingInterface::kPostLoad:
+        break;
+    case SKSE::MessagingInterface::kPreLoadGame:
+        break;
+    case SKSE::MessagingInterface::kPostLoadGame:
+        break;
+    case SKSE::MessagingInterface::kNewGame:
+        break;
+    }
 }
 
-SKSEPluginLoad(const SKSE::LoadInterface *skse) {
+SKSEPluginLoad(const SKSE::LoadInterface* skse) {
     SKSE::Init(skse);
-	SetupLog();
-
+    SetupLog();
 
     auto messaging = SKSE::GetMessagingInterface();
-	if (!messaging->RegisterListener("SKSE", MessageHandler)) {
-		return false;
-	}
+    if (!messaging->RegisterListener("SKSE", MessageHandler)) {
+        return false;
+    }
 
-	
     return true;
 }
